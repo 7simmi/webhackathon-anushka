7230f285733d77428cee3edb19d0463c699b7d09
anushka <manushka1219@gmail.com>
2022-11-30 16:06:16 +0530

    website completed

---
+++ vscode/script.js
@@ -0,0 +1,23 @@
+alert('test')
+
+
+:root{
+    --main-color: #e02f6b;
+    --blue: #0000ff;
+    --blue-dark: #18293c;
+
+
+
+}
+
+*{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    outline: none;
+
+}
+::before,
+::after(
+    box-sizing:border-box;
+)
